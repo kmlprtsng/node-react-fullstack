821da3c86e89c1eff5c01b404f16e8499af5e76e
Kamalpreet Singh Chauhan <kmlprtsng@gmail.com>
2017-12-08 05:55:30 +0000

    finish lecture 79

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,21 +1,17 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-  // normal code looks like this for returning an action creator with react redux
-  // but we are going to use redux-thunk to get direct access to the dispatcher.
-  // const request = axios.get("/api/user/current");
-  // return {
-  //     type: FETCH_USER,
-  //     payload: request
-  // }
-
-  return function(dispatch) {
-    axios.get("/api/user/current").then(res =>
-      dispatch({
-        type: FETCH_USER,
-        payload: res
-      })
-    );
-  };
+export const fetchUser = () => async dispatch => {
+  dispatch({
+    type: FETCH_USER,
+    payload: await axios.get("/api/user/current")
+  });
 };
+
+// normal code looks like this for returning an action creator with react redux
+// but we are going to use redux-thunk to get direct access to the dispatcher.
+// const request = axios.get("/api/user/current");
+// return {
+//     type: FETCH_USER,
+//     payload: request
+// }
